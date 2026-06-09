0ed6d67ebcccd140d2a911e0665d9581db85044b
Ricardo Fernández Chamorro <databaandsc@gmail.com>
2026-06-09 16:50:27 +0200

    chore: inicializar proyecto spring boot con flyway

---
+++ backend/andalucia_datahub_backend/src/test/java/com/andalucia/datahub_backend/AndaluciaDatahubBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.andalucia.datahub_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AndaluciaDatahubBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
